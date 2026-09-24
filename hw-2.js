14d2f4c92802ea0c001061902bd17217c892cd88
Alex7487 <kent7487@yandex.ru>
2026-09-24 21:35:58 +0500

    add 2 task

--- hw-2.js
+++ hw-2.js
@@ -1,8 +1,44 @@
-let password = 'пароль'
-let passwordUs = prompt('Введите пароль')
+let password = 'пароль';
+let passwordUs = prompt('Введите пароль');
 if (password === passwordUs) {
-    alert("Пароль введен верно")
+    alert("Пароль введен верно");
 }
 else {
     alert("Пароль введён неправильно")
+}
+
+let c = 0;
+
+if ((c > 0)  && (c < 10)) {
+    alert("Верно")
+}
+else {
+    alert("Не верно")
+}
+
+c = 10;
+
+if ((c > 0)  && (c < 10)) {
+    alert("Верно")
+}
+else {
+    alert("Не верно")
+}
+
+c = -3;
+
+if ((c > 0)  && (c < 10)) {
+    alert("Верно")
+}
+else {
+    alert("Не верно")
+}
+
+c = 2;
+
+if ((c > 0)  && (c < 10)) {
+    alert("Верно")
+}
+else {
+    alert("Не верно")
 }
