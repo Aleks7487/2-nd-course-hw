6e28549fa893bcd4296b59c1f6ae39a21b1e325b
Alex7487 <kent7487@yandex.ru>
2026-09-24 21:26:01 +0500

    add 1 task

---
+++ hw-2.js
@@ -0,0 +1,8 @@
+let password = 'пароль'
+let passwordUs = prompt('Введите пароль')
+if (password === passwordUs) {
+    alert("Пароль введен верно")
+}
+else {
+    alert("Пароль введён неправильно")
+}
