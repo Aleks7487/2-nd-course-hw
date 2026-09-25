88fae9601f1aea406fecc2552007a54619dea083
Alex7487 <kent7487@yandex.ru>
2026-09-25 21:05:29 +0500

    4 task

--- hw-4.js
+++ hw-4.js
@@ -2,9 +2,9 @@
 
 function less(a , b) {
     if (a >= b) {
-        return(b)
+        console.log(b)
     } else {
-        return(a)
+        console.log(a)
     }
 }
 
@@ -13,27 +13,53 @@ less(7 , 7)
 
 // 2 task
 
+
 function evenNumber(n) {
     if (n % 2 == 0) {
-        return('Число четное')
+        console.log('Число четное')
     } else {
-        return('Число нечетное')
+        console.log('Число нечетное')
     }
 }
+
+evenNumber(5)
+evenNumber(4)
 
 // 3 task
 
 // Напишите функцию, которая принимает параметром число и выводит в консоль квадрат этого числа.
 // Напишите функцию, которая принимает параметром число и возвращает квадрат этого числа значением — так, чтобы потом это значение можно было использовать.
 
-
 function square(e) {
     console.log(e ** 2)
 }
 
-function square2(params) {
+function square2(e) {
     return(e ** 2)
 }
 
-// 4 task
+square(4);
+square2(5)
 
+// // 4 task
+
+let userAge = prompt('Сколько вам лет')
+
+
+
+function age(fage) {
+    if (fage > 12) {
+        console.log('Добро пожаловать')
+    }
+    if (fage >= 0 && age <= 12) {
+        console.log('Привет, друг')
+    }
+    if (fage < 0) {
+        console.log('Вы ввели неправильное значение')
+    }
+    // else {
+    //     console.log('Вы ввели неправильное значение')
+    // }
+}
+
+age(userAge)
