6b35be87a1e686d5f9b4e5ad7902e82b625e23cc
Alex7487 <kent7487@yandex.ru>
2026-09-25 16:33:04 +0500

    add 1 and 2 task

---
+++ hw-4.js
@@ -0,0 +1,25 @@
+// 1 task
+
+function less(a , b) {
+    if (a >= b) {
+        return(b)
+    } else {
+        return(a)
+    }
+}
+
+less(4 , 6)
+less(7 , 7)
+
+// 2 task
+
+function evenNumber(n) {
+    if (n % 2 == 0) {
+        return('Число четное')
+    } else {
+        return('Число нечетное')
+    }
+}
+
+// 3 task
+
