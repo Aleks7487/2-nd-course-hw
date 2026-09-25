591c2c82c1bf8c0a04bc59ea2b087ea1cd8ca70a
Alex7487 <kent7487@yandex.ru>
2026-09-25 10:47:35 +0500

    add 2 and 3 task

--- hw-3.js
+++ hw-3.js
@@ -3,10 +3,27 @@
 let i = 1
 
 while (i < 3) {
-    alert("Привет")
+    console.log("Привет")
     i++
 }
 
 // 2 task
 
+i = 1
+
+while (i < 6) {
+    console.log(i)
+    i++
+}
+
+// 3 task
+
+i = 7
+
+while (i < 23) {
+    console.log(i)
+    i++
+}
+
+// 4 task
 
