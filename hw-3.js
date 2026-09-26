691495e0f229c21e9b129ea9fedac909a3150814
Alex7487 <kent7487@yandex.ru>
2026-09-26 11:18:51 +0500

    5 task

--- hw-3.js
+++ hw-3.js
@@ -36,3 +36,29 @@ const obj = {
 for (let key in obj) {
     console.log(`${key} — зарплата ${obj[key]} долларов`);
 }
+
+// 5 task 
+
+let n = 1000;
+let num = 0;
+
+while (n >= 50) {
+    n /= 2,
+    num++   
+}
+
+console.log('Итоговое число:', n);
+console.log('Количество итераций:', num);
+
+// решил попробовать проверить))
+
+// n = 31.25
+// num = 0
+
+// while (n < 1000) {
+//     n *= 2,
+//     num++
+// }
+
+// console.log('Итоговое число:', n);
+// console.log('Количество итераций:', num);
